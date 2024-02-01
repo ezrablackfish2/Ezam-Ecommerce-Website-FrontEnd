a8e95c711bc601239041b715d610331b4fd2287e
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-02-01 14:44:58 +0300

    truly clapped now

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,41 @@ import { useDispatch, useSelector } from "react-redux";
 import { setProductsData } from "../features/productSlice";
 import { useEffect } from "react";
 import FetchProduct from "../fetch/product";
+import ezra from "../public/ezra.jpg";
+import amanuel from "../public/amanuel.webp";
 
 function Home() {
 	const dispatch = useDispatch();
 	const products = useSelector(state => state.products.productsData);
 	FetchProduct();
 
-	console.log(products);
 	return(
-	<>
-		<div>Home Page Of Ezam website </div>
-		<div>
+		<div className={styles.home}>
+		<div className={styles.promotion}>Unlock premium products at unbeatable prices! Shop now at EZam Ecommerce for high-quality goods without breaking the bank</div>
+		<div className={styles.category}>
+		<div className={styles.categoryTitle}>
+		<span className={styles.storeText}>Shop. </span> ultimate destination for purchasing items you adore.
+		</div>
+		<div className={styles.specialist}>
+		<img
+			className={styles.specialistImage}
+			src={ezra}
+		/>
+		<img
+			className={styles.specialistImage}
+			src={amanuel}
+		/>
+
+		<div className={styles.upperSpecialistText}>
+		Need shopping help?
+		</div>
+		<div className={styles.lowerSpecialistText}>
+		<a className={styles.specialistLink} href="#">
+		Ask Owners
+		</a>
+		</div>
+		</div>
+		</div>
             	    {products.map((product, index) => (
 			<div key={product.id}>
             	        <div> Name {product.name}</div>
@@ -21,7 +45,6 @@ function Home() {
 			</div>
             	    ))}
             	</div>
-	</>
 	)
 }
 
