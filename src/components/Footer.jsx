7d2ceffa43c56e273dbaa2ee284bd0a3a1ed8b17
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-02-03 13:44:50 +0300

    intenstive css viewing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,25 @@ import styles from "./Footer.module.css";
 
 
 const Footer = () => {
+	const footerData = [
+      ['Row 1, Column 1', 'Row 1, Column 2', 'Row 1, Column 3', 'Row 1, Column 4'],
+      ['Row 2, Column 1', 'Row 2, Column 2', 'Row 2, Column 3', 'Row 2, Column 4'],
+      ['Row 3, Column 1', 'Row 3, Column 2', 'Row 3, Column 3', 'Row 3, Column 4'],
+      ['Row 4, Column 1', 'Row 4, Column 2', 'Row 4, Column 3', 'Row 4, Column 4']
+    ];
 
 	return (
 		<div className={styles.footer}>
 			<table className={styles.table}>
-				<tr>
-					<td className={styles.cell}>Resources</td>
-					<td className={styles.cell}>Help</td>
-					<td className={styles.cell}>Company</td>
-					<td className={styles.cell}>Promotion & Discounts</td>
-				</tr>
+				<tbody>
+			          {footerData.map((row, rowIndex) => (
+			            <tr key={rowIndex}>
+			              {row.map((cell, cellIndex) => (
+			                <td className={styles.cell}key={cellIndex}>{cell}</td>
+			              ))}
+			            </tr>
+			          ))}
+			        </tbody>
 			</table>
 		</div>	
 	);
