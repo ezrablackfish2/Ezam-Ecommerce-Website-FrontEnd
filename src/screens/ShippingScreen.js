d584a88b44ead17ba925142259cad6bc51e7eb75
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-02-09 15:58:50 +0300

    form is done css

--- src/screens/ShippingScreen.js
+++ src/screens/ShippingScreen.js
@@ -5,6 +5,8 @@ import FormContainer from '../components/FormContainer'
 import CheckoutSteps from '../components/CheckoutSteps'
 import { saveShippingAddress } from '../actions/cartActions'
 import { useNavigate } from "react-router-dom"
+import styles from "../components/Form.module.css"
+
 
 function ShippingScreen() {
 
@@ -27,65 +29,56 @@ function ShippingScreen() {
     }
 
     return (
-        <FormContainer>
-            <CheckoutSteps step1 step2 />
-            <h1>Shipping</h1>
-            <Form onSubmit={submitHandler}>
+        <div className={styles.totalForm}>
+            <div>
+                <div className={styles.formTitle}>Shipping</div>
+                <form onSubmit={submitHandler}>
 
-                <Form.Group controlId='address'>
-                    <Form.Label>Address</Form.Label>
-                    <Form.Control
-                        required
-                        type='text'
-                        placeholder='Enter address'
-                        value={address ? address : ''}
-                        onChange={(e) => setAddress(e.target.value)}
-                    >
-                    </Form.Control>
-                </Form.Group>
+                    <div>
+                        <label className={styles.label} htmlFor='address'>Address</label>
+                        <input
+	    		    className={styles.input}
+                            required
+                            type='text'
+                            id='address'
+                            placeholder='Enter address'
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
+                        />
+                    </div>
 
-                <Form.Group controlId='city'>
-                    <Form.Label>City</Form.Label>
-                    <Form.Control
-                        required
-                        type='text'
-                        placeholder='Enter city'
-                        value={city ? city : ''}
-                        onChange={(e) => setCity(e.target.value)}
-                    >
-                    </Form.Control>
-                </Form.Group>
+                    <div>
+                        <label className={styles.label} htmlFor='city'>City</label>
+                        <input
+	    		    className={styles.input}
+                            required
+                            type='text'
+                            id='city'
+                            placeholder='Enter city'
+                            value={city}
+                            onChange={(e) => setCity(e.target.value)}
+                        />
+                    </div>
 
-                <Form.Group controlId='postalCode'>
-                    <Form.Label>Postal Code</Form.Label>
-                    <Form.Control
-                        required
-                        type='text'
-                        placeholder='Enter postal code'
-                        value={postalCode ? postalCode : ''}
-                        onChange={(e) => setPostalCode(e.target.value)}
-                    >
-                    </Form.Control>
-                </Form.Group>
-
-                <Form.Group controlId='country'>
-                    <Form.Label>Country</Form.Label>
-                    <Form.Control
-                        required
-                        type='text'
-                        placeholder='Enter country'
-                        value={country ? country : ''}
-                        onChange={(e) => setCountry(e.target.value)}
-                    >
-                    </Form.Control>
-                </Form.Group>
-
-                <Button type='submit' variant='primary'>
-                    Continue
-                </Button>
-            </Form>
-        </FormContainer>
-    )
-}
+                    <div>
+                        <label className={styles.label} htmlFor='postalCode'>Postal Code</label>
+                        <input
+	    		    className={styles.input}
+                            required
+                            type='text'
+                            id='postalCode'
+                            placeholder='Enter postal code'
+                            value={postalCode}
+                            onChange={(e) => setPostalCode(e.target.value)}
+                        />
+                    </div>
+			
+	    	    <div className={styles.buttonContainer}>
+                    <button className={styles.Button} type='submit'>Continue</button>
+	    	    </div>
+                </form>
+            </div>
+        </div>
+    );}
 
 export default ShippingScreen
